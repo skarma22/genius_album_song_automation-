4c7e9d921d098b06ecccfb79463404f105309809
skarma22 <skarma615@gmail.com>
2024-11-12 15:40:39 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo = " BUILD START"
-python3.9 -m pip install -r requirements.text
-python3.9 mannage.py collectstatic --noinput --clear
+python3.12 -m pip install -r requirements.text
+python3.12 mannage.py collectstatic --noinput --clear
 echo " BUILD END"
